c6fcaa415329ea4d7453aec7f90d6bd42ca45eeb
Tyrell-Lewis <tyrell.lewis@my.uwi.edu>
2025-04-17 02:47:03 +0000

    Removing unnecessary files from controllers.

--- App/controllers/__init__.py
+++ App/controllers/__init__.py
@@ -3,18 +3,10 @@ from .auth import *
 from .staff import *
 from .student import *
 from .karma import *
-from .accomplishment import *
 from .review import *
-from .recommendation import *
-from .incidentReport import *
-from .grades import *
 from .admin import *
 from .csv import *
 from .staffcsv import *
-from .transcript import *
-from .schoolRecommendation import *
-from .jobRecommendation import *
-from .badges import *
 from .comment import *
 from .reply import *
 
