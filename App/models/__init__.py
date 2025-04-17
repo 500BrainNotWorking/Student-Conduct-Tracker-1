5b5b77913603bd4c0f1dfdbc608d1952e9b39049
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-16 22:54:13 -0400

    Remove unused models

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -3,14 +3,6 @@ from .admin import *
 from .student import *
 from .staff import *
 from .review import *
-from .recommendation import *
-from .accomplishment import *
 from .karma import *
-from .incidentReport import *
-from .grades import *
-from .transcript import *
-from .schoolRecommendation import *
-from .jobRecommendation import *
-from .badges import *
 from .reply import *
 from .comment import *
--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ import subprocess
 
 from App.database import db, get_migrate
 from App.main import create_app
-from App.models import Student, Karma, Badges, Accomplishment, Comment
+from App.models import Student, Karma, Comment
 from App.models import User
 from App.controllers import (
     create_student, create_staff, create_admin, get_all_users_json,
